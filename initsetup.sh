a53a9c3e4a1a067cd559541274ed3676d8328e1f
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2023-01-30 16:34:08 +0900

    init commit

---
+++ initsetup.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+##sudo apt-get install upgrade 
+echo 'Installing ssh, vim, wget, zip, unzip, curl'
+sudo apt-get install -y ssh
+sudo apt-get install -y vim
+sudo apt-get install -y wget
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
+sudo apt-get install -y curl 
+
+echo 'sudo apt-get install update'
+
+sudo apt-get install update 
+
+echo 'Remember to install 
+-> RabbitMQ 
+-> Unity Launcher Folders 
+-> Linux Intel Graphics'
