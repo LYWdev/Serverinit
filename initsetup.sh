0effd31bd9e00f7e4e504ba9124af3d783ddb5e9
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2023-01-30 18:18:37 +0900

    add virsh console vmname

--- initsetup.sh
+++ initsetup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y wget
 sudo apt-get install -y zip
 sudo apt-get install -y unzip
 sudo apt-get install -y curl 
-sudo apt-get install -y  
+sudo apt-get install -y xscreensaver
 
 echo 'sudo apt-get install update'
 
@@ -18,3 +18,11 @@ echo 'Remember to install
 -> RabbitMQ 
 -> Unity Launcher Folders 
 -> Linux Intel Graphics'
+
+echo 'Screen Saver Off'
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+echo 'VM Console service enable'
+sudo systemctl status serial-getty@ttyS0.service
+systemctl enable serial-getty@ttyS0.service
+systemctl start serial-getty@ttyS0.service
