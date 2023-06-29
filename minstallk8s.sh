1c26ad078823f2348ba2a86035a2a78d24843344
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2023-06-30 03:01:31 +0900

    edit winstallk8s.sh

--- minstallk8s.sh
+++ minstallk8s.sh
@@ -17,7 +17,6 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
-
 sleep 1
 echo **********package installed**********
 
@@ -25,12 +24,18 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#Docker, containerd 설치
+echo **********NOW : ADD REPO**********
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo **********END : ADD REPO**********
+echo **********INSTALL : CONTAINER D, DOCKER  **********
+
+#Docker, containerd 설치
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
